028b8c1cba4d4e7866a9d5c19afc819f894bbe07
João Fontes <j.henry.kings@gmail.com>
2025-02-18 18:28:03 +0000

    metode to create jwt

--- src/main/java/com/example/jwtTest/security/JwtUtil.java
+++ src/main/java/com/example/jwtTest/security/JwtUtil.java
@@ -1,5 +1,7 @@
 package com.example.jwtTest.security;
 
+import java.security.Signature;
+
 public class JwtUtil {
     @Value("${jwt.secret}")
     private String secret;
@@ -7,11 +9,24 @@ public class JwtUtil {
     @Value("${jwt.expiration}")
     private Long expirationTime;
     
+    //metodo para gerar token
     public String generatedToken(String username){
         return jwsts.builder()
         .setSubject(username)
         .setIssuedAt(new Date())
+        .setExpiration((new Date(System.currentTimeMillis() + expirationTime)))
+        .signWith(SignatureAlgorithm.HS256, secret)
+        .compact();
 
+    }
+
+    public boolean isTokenValid(String token){
+        try {
+            jwts.parser().setSigningKey(secret).parseClaimsJws((token));
+            return true;
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
     }
 
 }
